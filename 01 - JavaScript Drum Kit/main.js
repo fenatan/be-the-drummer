8648b51dcf42abe765b9a2b3daaadd497adc3bec
Felipe Natan <felipenatan@novaandradina.org>
2020-05-05 22:03:12 -0400

    feature: add backtracking

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -3,7 +3,7 @@ window.addEventListener('keydown', (e) => {
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return;
     audio.currentTime = 0;
-    audio.volume = 0.4;
+    // audio.volume = 0.4;
     audio.play();
     key.classList.add('active');
     setTimeout(() => {
